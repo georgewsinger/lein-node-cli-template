0d324120861e7453d553ad1ddf95f08fb7fafc4f
George Singer <george.w.singer@gmail.com>
2016-02-07 07:09:09 -0500

    Changed notation to <cb1, <cb2, <cb3, <cb?, cb>1, cb>2, cb>3, cb>?.

--- src/leiningen/new/node_cli/cb-heaven-core.cljs
+++ src/leiningen/new/node_cli/cb-heaven-core.cljs
@@ -26,12 +26,12 @@
 ;; that generate both a success value and an error value at the same time.
 
 ;; this fn equivalent to (>? ..) when parent fn executes cb with only one argument
-(defn >1 
+(defn cb>1 
   "If at all possible, jams the 1st callback argument into the input channel."
   ([c] (fn [arg1] (go (>! c (chan-sanitized arg1)))))
-  ([c E-msg] (fn [arg1] (go (if arg1 (>! c (chan-sanitized arg1)) (>! c E-msg))))))
+  ([c E-msg] (fn [arg1] (go (if arg1 (>! c (chan-sanitized arg1)) (>! c (js/Error. E-msg)))))))
 
-(defn >2
+(defn cb>2
   "If at all possible, jams the 2nd callback argument into the input channel."
   ([c]
    (fn [err, res] (go (cond
@@ -40,9 +40,9 @@
   ([c E-msg]
    (fn [err, res] (go (cond
                        res     (>! c (chan-sanitized res))
-                       :else   (>! c (chan-sanitized E-msg)))))))
+                       :else   (>! c (chan-sanitized (js/Error. E-msg))))))))
 
-(defn >3
+(defn cb>3
   "If at all possible, jams the 3rd callback argument into the input channel."
   ([c]
    (fn [err1, err2, res] (go (cond
@@ -52,11 +52,11 @@
   ([c E-msg]
    (fn [err1, err2, res] (go (cond
                               res   (>! c (chan-sanitized res))
-                              err1   (>! c (chan-sanitized E-msg))
+                              err1   (>! c (chan-sanitized (js/Error. E-msg)))
                               :else  (>! c (chan-sanitized E-msg)))))))
 
 ;; This function generates an [error-first callback](http://fredkschott.com/post/2014/03/understanding-error-first-callbacks-in-node-js/)
-(defn >? 
+(defn cb>? 
   "Jams the first truthy argument of a callback function into a channel."
   ([c]
   (fn [& args] 
@@ -72,8 +72,15 @@
           (>! c E-msg)
           (if (first a)
               (if (> (count a) 1)
-                  (>! c E-msg)
+                  (>! c (js/Error. E-msg))
                   (>! c (first (chan-sanitized a))))
               (recur (rest a))))))))
+
+;; used with David Nodel's `<?` macro
+(defn throw-if-err
+  [x]
+  (if (instance? js/Error x)
+    (throw x)
+    x))
 
 (set! *main-cli-fn* -main)
--- src/leiningen/new/node_cli/cb-heaven-macros.clj
+++ src/leiningen/new/node_cli/cb-heaven-macros.clj
@@ -1,57 +1,64 @@
 (ns cljs-callback-heaven.macros)
 
-(defmacro <? 
+(defmacro <cb? 
   ([func]
     (let [c (gensym "c")]
       `(cljs.core.async.macros/go 
          (let [~c (cljs.core.async/chan 1)]
-           ~(replace {'_ `(cljs-callback-heaven.core/>? ~c)} func)
+           ~(replace {'_ `(cljs-callback-heaven.core/cb>? ~c)} func)
            (cljs.core.async/<! ~c)))))
   ([func E-msg]
     (let [c (gensym "c")]
       `(cljs.core.async.macros/go 
          (let [~c (cljs.core.async/chan 1)]
-           ~(replace {'_ `(cljs-callback-heaven.core/>? ~c ~E-msg)} func)
+           ~(replace {'_ `(cljs-callback-heaven.core/cb>? ~c ~E-msg)} func)
            (cljs.core.async/<! ~c))))))
 
-(defmacro <1 
+(defmacro <cb1 
   ([func]
     (let [c (gensym "c")]
       `(cljs.core.async.macros/go 
          (let [~c (cljs.core.async/chan 1)]
-           ~(replace {'_ `(cljs-callback-heaven.core/>1 ~c)} func)
+           ~(replace {'_ `(cljs-callback-heaven.core/cb>1 ~c)} func)
            (cljs.core.async/<! ~c)))))
   ([func E-msg]
     (let [c (gensym "c")]
       `(cljs.core.async.macros/go 
          (let [~c (cljs.core.async/chan 1)]
-           ~(replace {'_ `(cljs-callback-heaven.core/>1 ~c ~E-msg)} func)
+           ~(replace {'_ `(cljs-callback-heaven.core/cb>1 ~c ~E-msg)} func)
            (cljs.core.async/<! ~c))))))
 
-(defmacro <2 
+(defmacro <cb2 
   ([func]
     (let [c (gensym "c")]
       `(cljs.core.async.macros/go 
          (let [~c (cljs.core.async/chan 1)]
-           ~(replace {'_ `(cljs-callback-heaven.core/>2 ~c)} func)
+           ~(replace {'_ `(cljs-callback-heaven.core/cb>2 ~c)} func)
            (cljs.core.async/<! ~c)))))
   ([func E-msg]
     (let [c (gensym "c")]
       `(cljs.core.async.macros/go 
          (let [~c (cljs.core.async/chan 1)]
-           ~(replace {'_ `(cljs-callback-heaven.core/>2 ~c ~E-msg)} func)
+           ~(replace {'_ `(cljs-callback-heaven.core/cb>2 ~c ~E-msg)} func)
            (cljs.core.async/<! ~c))))))
 
-(defmacro <3 
+(defmacro <cb3 
   ([func]
     (let [c (gensym "c")]
       `(cljs.core.async.macros/go 
          (let [~c (cljs.core.async/chan 1)]
-           ~(replace {'_ `(cljs-callback-heaven.core/>3 ~c)} func)
+           ~(replace {'_ `(cljs-callback-heaven.core/cb>3 ~c)} func)
            (cljs.core.async/<! ~c)))))
   ([func E-msg]
     (let [c (gensym "c")]
       `(cljs.core.async.macros/go 
          (let [~c (cljs.core.async/chan 1)]
-           ~(replace {'_ `(cljs-callback-heaven.core/>3 ~c ~E-msg)} func)
+           ~(replace {'_ `(cljs-callback-heaven.core/cb>3 ~c ~E-msg)} func)
            (cljs.core.async/<! ~c))))))
+
+;; David Nolen's `<?` macro
+
+(defmacro <?
+  "Actively throw an exception if something goes wrong when waiting on a channel message."
+  [expr]
+  `(cljs-callback-heaven.core/throw-if-err (cljs.core.async/<! ~expr)))
