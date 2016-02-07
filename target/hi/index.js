e50ce7fc8d2eddfff85d5a163957a3d5dd81cfba
George Singer <george.w.singer@gmail.com>
2016-02-07 07:34:03 -0500

    Ready for repush.

--- target/hi/index.js
+++
@@ -1,4 +0,0 @@
-require('./out-none/goog/bootstrap/nodejs');
-require('./target/hi-none');
-require('./out-none/hi/core');
-hi.core._main(process.argv[2], process.argv[3], process.argv[4], process.argv[5], process.argv[6]);
--- target/hi/test/hi/runner.cljs
+++
@@ -1,5 +0,0 @@
-(ns hi.runner
-    (:require [doo.runner :refer-macros [doo-tests]]
-              [hi.core-test]))
-
-(doo-tests 'hi.core-test)
